9b55fecb8dd4085ecfd234cf4e58a03e208f6590
Brijbhan Chauhan <brijphy@gmail.com>
2021-03-29 11:29:37 +0530

    committing code

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ function cal(...rest) {
 		rest.reduce((acc,el) => acc + el,0 )
 	}
 } 	
- function SayHi () {
-console.log("he buddy");
+function SayHi () {
+	console.log("he buddy");
 	 console.log("hello, world ");
 	
 }
