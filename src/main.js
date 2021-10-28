d1b22fb91ca71fba5249ef0045c92d2cc85fa7f3
brijbhan <brijphy@gmail.com>
2021-10-28 10:42:16 -0500

    removing function from fre

--- src/main.js
+++ src/main.js
@@ -4,15 +4,11 @@ function SayHello () {
 }
 
 
-	// function comments
+//function comments
 // next comment my self
 // hi this is brijbhan chauhan
 
 	console.log("hello, world");
 }
-function xtRay(...rest) {
-		//test comments:
-		console.log("here is the fucntion name ");;
-	console.log(rest.reduce((acc,el)=>acc+el,0));
-}
+
 
