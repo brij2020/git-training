0b23fa23c0acc794b84b0e3bbece313a01c65cb1
Vibhan Chauhan <bvibha.chauhan@outlook.com>
2022-04-28 20:42:25 +0530

    new commit to branch

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ console.log('hello world');
 // adding feature in the branch 
 function foo() {
     console.log(`function is exiting in new `);
+    console.log(`Bar is new added in the feature branch.`)
+}
+function Bar() {
+    conso
 }
